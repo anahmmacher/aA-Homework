6fcb24bf48c07928277799aa2c9d1c80bd425d4d
anahmmacher <acnahmmacher@gmail.com>
2022-01-20 20:47:05 -0800

    updating repo

--- W4/W4D4/Rspec_homework/spec/dessert_spec.rb
+++ W4/W4D4/Rspec_homework/spec/dessert_spec.rb
@@ -6,42 +6,57 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "Arthur") }
+  let(:pie) {Dessert.new("chocolate pie", 5, chef)}
 
   describe "#initialize" do
     it "sets a type" do
-      expect(dessert.instance_variable_get(:@type)).to eq(type)
-
+      expect(pie.type).to eq("chocolate pie")
+    end
     it "sets a quantity" do
-      expect(dessert.instance_variable_get(:@quantity)).to eq(quantity)
+      expect(pie.quantity).to eq(5)
+    end
     it "starts ingredients as an empty array" do 
-      expect(dessert.instance_variable_get(:@ingredients).to eq([])
+      expect(pie.ingredients).to be_empty
+    end
     it "raises an argument error when given a non-integer quantity" do
-      expect {} to raise_error(ArgumentError)
+      expect {Dessert.new("chocolate pie", "five", chef)} to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
-    expect (dessert.add_ingredient(ingredient)).to 
+    it "adds an ingredient to the ingredients array" do
+    pie.add_ingredient("chocolate")
+    expect (pie.ingredients).to eq(["chocolate"])
+    end
   end
 
   describe "#mix!" do
     it "shuffles the ingredient array" do
-      @ingredients.shuffle!
+      pie.ingredients = ["chocolate", "flour", "sugar", "butter"]
+      expect(pie.mix!).to be(pie.ingredients.shuffle!)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-      @quantity -= amount
-    it "raises an error if the amount is greater than the quantity"
-      expect {amount > @quantity} to raise_error("not enough left!")
+    it "subtracts an amount from the quantity" do
+      expect(pie.eat(3)).to eq(2)
+    end
+    it "raises an error if the amount is greater than the quantity" do
+      expect {pie.eat(9)} to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(pie.serve).to eq("Chef Arthur the Great Baker has made 5 chocolate pies")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(pie)
+      pie.make_more
+    end
   end
 end
